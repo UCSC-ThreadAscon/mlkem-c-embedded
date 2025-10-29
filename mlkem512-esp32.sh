fa55eec4e952d8ff18e38eaa2bbb2572ee80ed86
Simeon Tran <4740112+SimeonAT@users.noreply.github.com>
2025-10-28 21:55:51 -0700

    Only works on the ESP32-C3

--- mlkem512-esp32.sh
+++ mlkem512-esp32.sh
@@ -1,5 +1,7 @@
 make PLATFORM=esp32-c3
-esptool.py -p /dev/cu.usbmodem11301 write_flash 0x0 build/esp32-c3/bin/mlkem512-nistkat.bin
-# esptool -p /dev/cu.usbmodem11301 write-flash 0x0 build/esp32-c3/bin/mlkem512-nistkat.bin
+esptool -p /dev/cu.usbmodem11301 write-flash 0x0 build/esp32-c3/bin/mlkem512-test.bin
 cd ../*test
-idf.py monitor --port /dev/cu.usbmodem11301 --baud 115200
+idf.py monitor --port /dev/cu.usbmodem11301 --monitor-baud 115200
+
+# esptool.py -p /dev/cu.usbmodem11301 write_flash 0x0 build/esp32-c3/bin/mlkem512-test.bin
+# pyserial-miniterm /dev/cu.usbmodem11301 115200
